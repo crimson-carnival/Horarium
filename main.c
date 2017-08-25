7b4d7e7bc11a2857adea7177110bbd4ede2f9c72
wraith-of-peril <vyommaitreya@gmail.com>
2017-08-25 12:14:25 +0530

    Testing integration in Slack

--- main.c
+++ main.c
@@ -4,6 +4,7 @@ int main() {
     int a;
     printf("Hello, World!\n");
     scanf("%d", &a);
-    printf("%d", a*a);
+    printf("%d\n", a*a);
+    printf("This is just a test commit.\n")
     return 0;
 }
