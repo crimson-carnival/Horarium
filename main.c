aed6627457df24478295b99837c96594aeed5f6d
wraith-of-peril <vyommaitreya@gmail.com>
2017-10-27 11:59:31 +0530

    rectified errors

--- main.c
+++ main.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
+#include<string.h>
 #include"assets.h"
 
 struct teacher_info *t = teacher;
 struct course_info *c = course;
 struct subject_info *s = subject;
-
-struct teacher_info *temp = t;
 
 void input_teacher(struct teacher_info *ptr)
 {
@@ -78,14 +77,14 @@ void reset_course(struct course_info *ptr)
 void timetable(struct course_info *ptr, int i, int j)
 {
     int index=0;
-    temp=t;
-    while(strcmp(t[index]->timetable[i][j],"Break")==0 && index<5) index++;
+    struct teacher_info *temp = t;
+    while(strcmp((temp+index)->timetable[i][j],"Break")==0 && index<5) index++;
     if(index<=4)
     {
-        if(s[index]->credits>0)
-        ptr[i][j]->timetable = t[index]->subject_code;
-        t[index]->timetable[i][j]=ptr->name;
-        s[index]->credits--;
+        if((s+index)->credits>0)
+        ptr->timetable[i][j] = (temp+index)->subject_code;
+        (temp+index)->timetable[i][j]=ptr->name;
+        (s+index)->credits--;
     }
     if(i==7 && j==4) return;
     else if(j==4) timetable(ptr, i+1, 0);
